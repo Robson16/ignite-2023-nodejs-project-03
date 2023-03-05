dd97b9ce57809cc22cc5efde8a0461aa49f7c92e
Robson H. Rodrigues <robhenrod@gmail.com>
2023-03-05 16:04:33 -0300

    NodeJS - Módulo 2: Estrutura da aplicação e Módulo 3: Streams no Noje.js

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -1,7 +1,31 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
-const server = http.createServer((request, response) => {
-  return response.end("Hello world!");
+const users = [];
+
+const server = http.createServer(async (request, response) => {
+  const { method, url } = request;
+
+  await json(request, response);
+
+  if (method === 'GET' && url === '/users') {
+    return response
+      .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = request.body;
+
+    users.push({
+      id: 1,
+      name,
+      email,
+    });
+
+    return response.writeHead(201).end();
+  }
+
+  return response.writeHead(404).end();
 });
 
 server.listen(3333);
